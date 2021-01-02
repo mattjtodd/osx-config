3255b9776102650cf01bd6be0218380edab397c0
Matt Todd <matt.j.todd@gmail.com>
2021-01-02 20:21:21 +0000

    Added new deps

--- brew.sh
+++ brew.sh
@@ -3,6 +3,35 @@
 set -e && \
   brew update && \
   brew upgrade && \
-  brew install autoconf bash binutils coreutils diffutils ed findutils flex gawk \
-    gnu-indent gnu-sed gnu-tar gnu-which gpatch grep gzip less m4 make nano \
-    screen watch wdiff wget && brew cleanup
+  brew install \
+	autoconf \
+	bash \
+	binutils \
+	coreutils \
+	curl \
+	diffutils \
+	ed \
+	findutils \
+	flex \
+	gawk \
+	gnu-indent \
+	gnu-sed \
+	gnu-tar \
+	gnu-which \
+	gpatch \
+	grep \
+	gzip \
+	jq \
+	less \
+	m4 \
+	make \
+	nano \
+	screen \
+	watch \
+	wdiff \
+	wget && \
+  brew install --cask \
+	docker \
+        iterm2 \
+	zoom && \
+  brew cleanup
